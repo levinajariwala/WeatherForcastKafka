b90bae55c5179c85e114f22cf73e729569e99ff9
levinajariwala <levinajariwala@gmail.com>
2023-11-16 21:20:19 +0000

    kafka change

--- src/main/scala/KafkaFunctionality/ReadFromKafka.scala
+++ src/main/scala/KafkaFunctionality/ReadFromKafka.scala
@@ -2,7 +2,10 @@ package KafkaFunctionality
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-import org.apache.commons.mail._
+//import org.apache.commons.mail._
+import javax.mail._
+import javax.mail.internet._
+import java.util.Properties
 object ReadFromKafka  {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("KafkaToJson").master("local[*]").getOrCreate()
@@ -64,16 +67,39 @@ object ReadFromKafka  {
 
   // Function to send email alert
   def sendEmailAlert(recipient: String, subject: String, body: String): Unit = {
-    val email = new SimpleEmail()
-    email.setHostName("smtp.gmail.com")
-    email.setSmtpPort(587)
-    email.setAuthenticator(new DefaultAuthenticator("15mscit026@gmail.com", "Local$16S"))
-    email.setSSLOnConnect(true)
-    email.setFrom("15mscit026@gmail.com")
-    email.setSubject(subject)
-    email.setMsg(body)
-    email.addTo(recipient)
-    email.send()
+//    val email = new SimpleEmail()
+//    email.setHostName("smtp.gmail.com")
+//    email.setSmtpPort(587)
+//    email.setAuthenticator(new DefaultAuthenticator("15mscit026@gmail.com", "Local$16S"))
+//    email.setSSLOnConnect(true)
+//    email.setFrom("15mscit026@gmail.com")
+//    email.setSubject(subject)
+//    email.setMsg(body)
+//    email.addTo(recipient)
+//    email.send()
+val properties = new Properties()
+    properties.put("mail.smtp.host", "smtp.gmail.com") // Replace with your SMTP host
+    properties.put("mail.smtp.port", "587") // Replace with your SMTP port
+    properties.put("mail.smtp.auth", "true") // If authentication is required
+    properties.put("mail.smtp.starttls.enable", "true") // If TLS/SSL is required
+
+    val session = Session.getDefaultInstance(properties, new Authenticator {
+      override def getPasswordAuthentication: PasswordAuthentication = new PasswordAuthentication("15mscit026@gmail.com", "Local$16S")
+      // Replace with your email and password for authentication
+    })
+
+    try {
+      val message = new MimeMessage(session)
+      message.setFrom(new InternetAddress("15mscit026@gmail.com@example.com")) // Replace with sender email
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("levinajariwala@gmail.com")) // Replace with recipient email
+      message.setSubject(subject)
+      message.setText(body)
+
+      Transport.send(message)
+      println("Email sent successfully!")
+    } catch {
+      case e: MessagingException => e.printStackTrace()
+    }
   }
 
 }
