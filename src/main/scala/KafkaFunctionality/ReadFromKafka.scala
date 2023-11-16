4e7d26ee9ddc8addaea46fd139bc99c9c39792a1
levinajariwala <levinajariwala@gmail.com>
2023-11-16 20:30:13 +0000

    kafka change

--- src/main/scala/KafkaFunctionality/ReadFromKafka.scala
+++ src/main/scala/KafkaFunctionality/ReadFromKafka.scala
@@ -43,9 +43,9 @@ object ReadFromKafka  {
       .start()
       .awaitTermination()
 
-//    // Check if the last message's wind is higher than 4
-//    val lastMessageWind = df.select("wind_mph").orderBy(desc("timestamp")).limit(1).first().getDouble(0)
-//    print(lastMessageWind)
+    // Check if the last message's wind is higher than 4
+    val lastMessageWind = df.select("wind_mph").orderBy(desc("timestamp")).limit(1).first().getDouble(0)
+    print(lastMessageWind)
 //    if (lastMessageWind > 4.0) {
 //      println("!!!!!!!!!!!!!!!!AAAALLLLEEERRRRTTTT!!!!!!!!!!!!!!!!!!!")
 //      // Send an email alert
@@ -63,17 +63,17 @@ object ReadFromKafka  {
   }
 
   // Function to send email alert
-//  def sendEmailAlert(recipient: String, subject: String, body: String): Unit = {
-//    val email = new SimpleEmail()
-//    email.setHostName("smtp.gmail.com")
-//    email.setSmtpPort(587)
-//    email.setAuthenticator(new DefaultAuthenticator("15mscit026@gmail.com", "Local$16S"))
-//    email.setSSLOnConnect(true)
-//    email.setFrom("15mscit026@gmail.com")
-//    email.setSubject(subject)
-//    email.setMsg(body)
-//    email.addTo(recipient)
-//    email.send()
-//  }
+  def sendEmailAlert(recipient: String, subject: String, body: String): Unit = {
+    val email = new SimpleEmail()
+    email.setHostName("smtp.gmail.com")
+    email.setSmtpPort(587)
+    email.setAuthenticator(new DefaultAuthenticator("15mscit026@gmail.com", "Local$16S"))
+    email.setSSLOnConnect(true)
+    email.setFrom("15mscit026@gmail.com")
+    email.setSubject(subject)
+    email.setMsg(body)
+    email.addTo(recipient)
+    email.send()
+  }
 
 }
