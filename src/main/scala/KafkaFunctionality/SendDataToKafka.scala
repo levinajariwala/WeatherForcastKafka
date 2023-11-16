8cacae05df28586c778ab1af3b4f944c990bc109
levinajariwala <levinajariwala@gmail.com>
2023-11-16 17:39:20 +0000

    kafka change

--- src/main/scala/KafkaFunctionality/SendDataToKafka.scala
+++ src/main/scala/KafkaFunctionality/SendDataToKafka.scala
@@ -1,12 +1,18 @@
 
 
 package KafkaFunctionality
-import org.apache.spark.sql.{DataFrame, SparkSession}
+
+//import scala.util.parsing.json.JSON
+
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 import java.io.{File, OutputStreamWriter}
 import okhttp3.{OkHttpClient, Request}
 import okio.Okio
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 object SendDataToKafka {
   def main(args: Array[String]): Unit = {
@@ -17,55 +23,94 @@ object SendDataToKafka {
 
     val client = new OkHttpClient()
 
-    // Specify the city, date, and language
-    // cities: london, liverpool, manchester, edinburgh, birmingham
-    val city = "liverpool"
-    val date = "2023-11-07"
-    val lang = "en"
-    val j_date = "08_nov"
-
-    val endpoint = buildEndpoint(city, date, lang)
-
-    // Add headers to the request
+    println("60")
+    val city = "london"
+    val endpoint = buildEndpoint1(city)
     val request = new Request.Builder()
       .url(s"https://weatherapi-com.p.rapidapi.com/$endpoint")
       .get()
-      .addHeader("X-RapidAPI-Key", "757121081cmsh825958e8ab97809p1d38fejsnd2789514b9be")
+      .addHeader("X-RapidAPI-Key", "2467bc2bd9mshfd02cfd8d4e5a77p13ba22jsn5eecabaf967e")
       .addHeader("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
-      .build()
+      .build();
 
-    val response = client.newCall(request).execute()
-    val messageDF: DataFrame = if (response.isSuccessful) {
-      val responseBody = response.body().string()
-      val filePath = s"/tmp/bduk1710/Levina/$city/${city}_$j_date.json"
-      //      saveStringAsJsonFile(responseBody, filePath)
-      saveStringAsJsonFileToHDFS(spark, responseBody, filePath)
+    val response = client.newCall(request).execute();
+    val responseBody = response.body().string()
+    print(responseBody)
 
-      println(s"API data for $city saved as JSON")
-      // Create a DataFrame with the file path
-      println(filePath)
-      val filePathSeq = Seq((filePath))
-      println("48")
-      import spark.implicits._
-      //      println("50")
-      filePathSeq.toDF("File")
-//      println("52")
-    } else {
-      println(s"API request for $city was not successful. Response code: ${response.code()}")
-      println(s"Response body: ${response.body().string()}")
-      val emptySeq = Seq(("", "No File"))
-      import spark.implicits._
-      emptySeq.toDF("File")
-    }
-    println("60")
+    // Parse the JSON string
+//    val dfFromText = JSON.parseFull(responseBody)
+//    val dfFromText: Any = spark.read.json(Seq(responseBody).toDS)
+
+    val schema = StructType(
+      Array(
+        StructField("location", StructType(
+          Array(
+            StructField("name", StringType),
+            StructField("region", StringType),
+            StructField("country", StringType),
+            StructField("lat", DoubleType),
+            StructField("lon", DoubleType),
+            StructField("tz_id", StringType),
+            StructField("localtime_epoch", LongType),
+            StructField("localtime", StringType)
+          )
+        )),
+        StructField("current", StructType(
+          Array(
+            StructField("last_updated_epoch", LongType),
+            StructField("last_updated", StringType),
+            StructField("temp_c", DoubleType),
+            StructField("temp_f", DoubleType),
+            StructField("is_day", IntegerType),
+            StructField("condition", StructType(
+              Array(
+                StructField("text", StringType),
+                StructField("icon", StringType),
+                StructField("code", IntegerType)
+              )
+            )),
+            StructField("wind_mph", DoubleType),
+            StructField("wind_kph", DoubleType),
+            StructField("wind_degree", IntegerType),
+            StructField("wind_dir", StringType),
+            StructField("pressure_mb", DoubleType),
+            StructField("pressure_in", DoubleType),
+            StructField("precip_mm", DoubleType),
+            StructField("precip_in", DoubleType),
+            StructField("humidity", IntegerType), // Extracting humidity
+            StructField("cloud", IntegerType),
+            StructField("feelslike_c", DoubleType),
+            StructField("feelslike_f", DoubleType),
+            StructField("vis_km", DoubleType),
+            StructField("vis_miles", DoubleType),
+            StructField("uv", DoubleType),
+            StructField("gust_mph", DoubleType),
+            StructField("gust_kph", DoubleType)
+          )
+        ))
+      )
+    )
+    import spark.implicits._
+    // Read JSON string as DataFrame using the defined schema
+    val dfFromText: DataFrame = spark.read.schema(schema).json(Seq(responseBody).toDS)
+    println(dfFromText)
+    // Selecting required fields and extracting wind and humidity
+    val extractedDF = dfFromText.select(
+      $"current.wind_mph".alias("wind_mph"),
+      $"current.humidity".alias("humidity")
+    )
+
+    // Assuming you want to create a messageDF with specific columns and pass wind and humidity
+    val messageDF = extractedDF.select(
+      $"wind_mph", $"humidity")
     val kafkaServer: String = "ip-172-31-3-80.eu-west-2.compute.internal:9092"
     val topicSampleName: String = "weather_forecast"
     println("63")
     messageDF.show()
-    messageDF.select("File")
+//    messageDF.select("File")
     messageDF
       .write
-//      .format("kafka")
+      .format("kafka")
       .option("kafka.bootstrap.servers", kafkaServer)
       .option("topic", topicSampleName)
       .save()
@@ -77,39 +122,8 @@ object SendDataToKafka {
   }
 
 
-  //Function to build the api endpoint URL
-  def buildEndpoint(city: String, date: String, lang: String): String = {
-    s"history.json?q=$city&dt=$date&lang=$lang"
+  def buildEndpoint1(city: String): String = {
+    s"current.json?q=$city"
   }
-
-  // Function to save a string as JSON in a file
-  def saveStringAsJsonFile(data: String, filePath: String): Unit = {
-    val file = new File(filePath)
-    val sink = Okio.buffer(Okio.sink(file))
-    sink.writeUtf8(data)
-    sink.close()
-  }
-
-  // Function to save a string as JSON in a file in HDFS
-  def saveStringAsJsonFileToHDFS(spark: SparkSession, data: String, filePath: String): Unit = {
-    import org.apache.hadoop.fs.{FileSystem, Path}
-    val hadoopConf = spark.sparkContext.hadoopConfiguration
-    val hdfs = FileSystem.get(hadoopConf)
-
-    val outputStream = hdfs.create(new Path(filePath))
-    val writer = new OutputStreamWriter(outputStream)
-
-    try {
-      writer.write(data)
-      println(s"API data saved to HDFS at: $filePath")
-    } catch {
-      case e: Exception =>
-        println(s"Error saving data to HDFS: ${e.getMessage}")
-    } finally {
-      writer.close()
-      hdfs.close()
-    }
-  }
-
 
 }
