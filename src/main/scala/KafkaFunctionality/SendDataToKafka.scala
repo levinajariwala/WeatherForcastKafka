e0067bc1a23a23c525e860061d439f67948be008
levinajariwala <levinajariwala@gmail.com>
2023-11-17 10:03:32 +0000

    kafka change

--- src/main/scala/KafkaFunctionality/SendDataToKafka.scala
+++ src/main/scala/KafkaFunctionality/SendDataToKafka.scala
@@ -123,7 +123,7 @@ object SendDataToKafka {
         .save()
 
       println("Message is loaded to Kafka topic")
-      Thread.sleep(1000000) // Wait for 10 seconds before making the next call
+      Thread.sleep(100000) // Wait for 10 seconds before making the next call
 
 //      spark.stop()
     }
